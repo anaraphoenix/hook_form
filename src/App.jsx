cf275151c2f3f01b2bc11c47b42468634a6779f6
Amanda Bradburn <amandabradburn21@gmail.com>
2023-06-16 10:22:54 -0700

    added more forms to hook forms

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import './App.css';
 import Form from './components/form';
 
+
+
+
 function App() {
   return (
     <div className="App">
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,55 +1,79 @@
 import React, {useState} from 'react';
 
-const Form = () => {
+const Form = (props) => {
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false); 
 
     const createUser = (event) => {
         event.preventDefault();
-        const newUser = {firstName, lastName, email, password,confirmPassword};
-        console.log("Welcome", newUser)
         setFirstName("");
         setLastName("");
         setEmail("");
         setPassword("");
         setConfirmPassword("");
+        setHasBeenSubmitted( true );
     };
-
+    const formMessage = () => {
+        if( hasBeenSubmitted ) {
+            return "Thank you for submitting the form!";
+	}   else {
+            return "Welcome, please submit the form";
+	}
+};
+    
     return(
         <div className="wrapper">
-            <form onSubmit={createUser} className='form'>
-                <div className='text-box'>
-                    <label>First name: </label>
-                    <input type="text" value={firstName} onChange={ (event) => setFirstName(event.target.value)} />
-                </div>
-                <div className='text-box'>
-                    <label>Last name: </label>
-                    <input type="text" value={lastName} onChange={ (event) => setLastName(event.target.value)} />
-                </div>
-                <div className='text-box'>
-                    <label>Email: </label>
-                    <input type="text" value={email} onChange={ (event) => setEmail(event.target.value)} />
-                </div>
-                <div className='text-box'>
-                    <label>Password: </label>
-                    <input type="text" value={password} onChange={ (event) => setPassword(event.target.value)} />
-                </div>
-                <div className='text-box'>
-                    <label>Confirm Password: </label>
-                    <input type="text" value={confirmPassword} onChange={ (event) => setConfirmPassword(event.target.value)} />
-                </div>
-                <input type="submit" value="Create User" />
-            </form>
+            <div className='form-input'>
+                <form onSubmit={createUser} className='form'>
+                <h3>{ formMessage() }</h3>
+                    <div className='text-box'>
+                        <label>First name: </label>
+                        <input type="text" value={firstName} onChange={ (event) => setFirstName(event.target.value)}/>
+                        {firstName.length < 2 ? (
+                            <p>First name must be at least 2 characters!</p>
+                        ): null}
+                    </div>
+                    <div className='text-box'>
+                        <label>Last name: </label>
+                        <input type="text" value={lastName} onChange={ (event) => setLastName(event.target.value)}/>
+                        {lastName.length < 2 ? (
+                            <p>Last name must be at least 2 characters!</p>
+                        ): null}
+                    </div>
+                    <div className='text-box'>
+                        <label>Email: </label>
+                        <input type="text" value={email} onChange={ (event) => setEmail(event.target.value)}/>
+                        {email.length < 5 ? (
+                            <p>Email address must be at least 5 characters!</p>
+                        ): null}
+                    </div>
+                    <div className='text-box'>
+                        <label>Password: </label>
+                        <input type="password" value={password} onChange={ (event) => setPassword(event.target.value)} />
+                        {password.length < 8 ? (
+                            <p>Password must be at least 8 characters!</p>
+                        ): null}
+                    </div>
+                    <div className='text-box'>
+                        <label>Confirm Password: </label>
+                        <input type="password" value={confirmPassword} onChange={ (event) => setConfirmPassword(event.target.value)}/>
+                        {confirmPassword !== password ? 
+                        <p>Password and confirm password do not match!</p>:null}
+                    </div>
+                        <input type="submit" value="Create User"/>
+                </form>
+            </div>
             <div className='output'>
                 <h4>Your Form Data</h4>
-                <p>First Name {firstName} </p>
-                <p>Last Name {lastName} </p>
-                <p>Email {email} </p>
-                <p>Password {password}</p>
-                <p>Confirm Password {confirmPassword}</p>
+                <p>First Name <span> {firstName} </span></p> 
+                <p>Last Name <span> {lastName} </span></p>
+                <p>Email <span> {email} </span></p>
+                <p>Password <span> {password} </span></p>
+                <p>Confirm Password <span> {confirmPassword} </span></p>
             </div>
         </div>
 
